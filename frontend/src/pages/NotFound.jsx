385ecd1144a64f76855d77eeee874c7b84ffbeb0
YoussefAhmede <ya801393@gmail.com>
2024-09-21 23:17:52 +0300

    Initial commit

---
+++ frontend/src/pages/NotFound.jsx
@@ -0,0 +1,8 @@
+function NotFound() {
+    return <div>
+        <h1>404 Not Found</h1>
+        <p>Now you are lost in the desert. <br />Ride the camel and return <a href="http://localhost:5173/login">home</a>.</p>
+    </div>
+}
+
+export default NotFound
